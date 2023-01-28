b99106ab4a26bd831ef57dfa674f7edc1a3a5e17
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-28 01:11:03 +0100

    Validate NuevoPresupuesto.jsx

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -2,18 +2,26 @@ import { useState } from "react";
 
 import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({ 
+    presupuesto, 
+    setPresupuesto,
+    setIsValidPresupuesto 
+}) => {
 
     const [mensaje, setMensaje] = useState('');
 
     const handlePresupuesto = (e) => {
         e.preventDefault();
 
-        if( !Number(presupuesto) || Number(presupuesto) < 0 ) {
+        if( !presupuesto || presupuesto < 0 ) {
             setMensaje( 'No es un presupuesto válido' )
-        } else { 
-            console.log('Number');
+
+            return;
         }
+
+        setMensaje('');
+        setIsValidPresupuesto(true);
+        
     }
 
     return (
@@ -25,10 +33,10 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
 
                     <input
                         className="nuevo-presupuesto" 
-                        type="text" 
+                        type="number" 
                         placeholder="Añade tu Presupuesto"
                         value={ presupuesto }
-                        onChange={ e => setPresupuesto( e.target.value ) }
+                        onChange={ e => setPresupuesto(Number(e.target.value))}
                     />
 
                     <input type="submit" value='Añadir' />
