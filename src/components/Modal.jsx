977c9f1dc5d5dade6236e5e593c8743244289733
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-01-29 01:03:40 +0100

    Modal form DONE

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -24,6 +24,48 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
             <form className={ `formulario ${ animarModal ? 'animar' : 'cerrar' }` }>
                 <legend>Nuevo Gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre Gasto</label>
+
+                    <input 
+                        id='nombre'
+                        type="text" 
+                        placeholder='ej. Netflix'
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+
+                    <input 
+                        id='cantidad'
+                        type="number" 
+                        placeholder='ej. 75'
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Categoría</label>
+
+                    <select
+                        id='categoria'
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="servicios">Servicios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                        <option value="gastos">Gastos Varios</option>
+                    </select>
+                </div>
+
+                <input
+                    type='submit'
+                    value='Confirmar'
+                />
             </form>
         </div>
     )
