fd8d8ad2f47e9ff19f120f7e53c54ac3984cf069
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-02 01:17:43 +0100

    Edit ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import ListadoGastos from './components/ListadoGastos';
@@ -17,6 +17,16 @@ function App() {
 
   const [ modal, setModal ] = useState(false);
   const [ animarModal, setAnimarModal ] = useState(false);
+
+  const [ gastoEditar, setGastoEditar ] = useState({});
+
+  useEffect( () => {
+    if ( Object.keys(gastoEditar).length > 0  ) {
+      
+      handleNuevoGasto();
+
+    }
+  }, [ gastoEditar ]);
 
   const handleNuevoGasto = () => {
 
@@ -57,6 +67,7 @@ function App() {
           <main>
             <ListadoGastos 
               gastos={ gastos }
+              setGastoEditar={ setGastoEditar }
             />
           </main>
 
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -28,13 +28,13 @@ const diccionarioIconos = {
 }
 
 
-const Gasto = ({ gasto }) => {
+const Gasto = ({ gasto, setGastoEditar }) => {
 
     const { categoria, nombre, cantidad, id, fecha } = gasto;
 
     const leadingActions = () => (
         <LeadingActions>
-            <SwipeAction onClick={ () => console.log('editar...') }>
+            <SwipeAction onClick={ () => setGastoEditar( gasto )}>
                 Editar
             </SwipeAction>
         </LeadingActions>
