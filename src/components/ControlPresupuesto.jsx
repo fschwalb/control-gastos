9672143e2e424fb6a57d4bd412675c01daa32e3d
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-11 17:17:43 +0100

    Added ProgressBar Library

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+
+import 'react-circular-progressbar/dist/styles.css';
 
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
+    const [ porcentaje, setPorcentaje ] = useState(0);
     const [ disponible, setDisponible ] = useState(0);
     const [ gastado, setGastado ] = useState(0);
 
@@ -12,8 +16,15 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
         const totalDisponible = presupuesto - totalGastado;
 
+        // Calcular Porcentaje Gastado
+        const nuevoPorcentaje = (( (presupuesto - totalDisponible) / presupuesto ) * 100).toFixed(2);
+
         setDisponible( totalDisponible );
         setGastado( totalGastado );
+
+        setTimeout( () => {
+            setPorcentaje( nuevoPorcentaje );
+        }, 800);
 
     }, [ gastos ]);
     
@@ -30,7 +41,13 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
-                <p>Grafica aqui</p>
+                <CircularProgressbar 
+                    styles={ buildStyles({
+                        pathColor: '#3B82F6',
+                        trailColor: '#F5F5F5'
+                    })}
+                    value={ porcentaje }
+                />
             </div>
 
             <div className="contenido-presupuesto">
