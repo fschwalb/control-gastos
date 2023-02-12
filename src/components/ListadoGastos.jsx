e9eb89dc471db3a1d74fc481e030758a5e71e860
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-02-12 01:33:11 +0100

    Filtro FINISH

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -9,26 +9,31 @@ const ListadoGastos = ({
     }) => {
     return (
         <div className="listado-gastos contenedor">
-            <h2>{ gastos.length ? 'Gastos' : 'No hay gastos aún' }</h2>
 
             { filtro ? (
-                    gastosFiltrados.map( gasto => (
-                        <Gasto 
-                            key={ gasto.id }
-                            gasto={ gasto }
-                            setGastoEditar={ setGastoEditar }
-                            eliminarGasto={ eliminarGasto }
-                        />
-                    ))
+                    <>
+                        <h2>{ gastosFiltrados.length ? 'Gastos' : 'No se encontraron gastos en esta categoría' }</h2>
+                        { gastosFiltrados.map( gasto => (
+                            <Gasto 
+                                key={ gasto.id }
+                                gasto={ gasto }
+                                setGastoEditar={ setGastoEditar }
+                                eliminarGasto={ eliminarGasto }
+                            />
+                        ))}
+                    </>
                 ) : ( 
-                    gastos.map( gasto => (
-                        <Gasto 
-                            key={ gasto.id }
-                            gasto={ gasto }
-                            setGastoEditar={ setGastoEditar }
-                            eliminarGasto={ eliminarGasto }
-                        />
-                    ))
+                    <>
+                        <h2>{ gastos.length ? 'Gastos' : 'No hay gastos aún' }</h2>
+                        { gastos.map( gasto => (
+                            <Gasto 
+                                key={ gasto.id }
+                                gasto={ gasto }
+                                setGastoEditar={ setGastoEditar }
+                                eliminarGasto={ eliminarGasto }
+                            />
+                        ))}
+                    </>
                 )
             }
         </div>
